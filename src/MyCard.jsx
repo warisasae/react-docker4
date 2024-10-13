78131290debcaba50a511c2db76ec9e2484a9d0f
warisasae <152458740+warisasae@users.noreply.github.com>
2024-10-13 15:59:49 +0700

    edit

--- src/MyCard.jsx
+++ src/MyCard.jsx
@@ -28,6 +28,7 @@ export default function MyCard(props) {
   <CardActions>
     <Button size="small">xxx</Button>
     <Button size="small">Learn More</Button>
+    <Button size="small">223</Button>
   </CardActions>
 </Card>
 
